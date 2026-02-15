723dd4898c9e982514b2146df24e3f2c73c2ae9d
Esteban Kim <47050615+Esteban-Kim@users.noreply.github.com>
2026-02-15 08:07:03 -0700

    Time: 7 ms (35.95%) | Memory: 47.8 MB (25.80%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,41 +1,41 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> mapping = new HashMap<>();
-        int[] inward = new int[numCourses];
+        Map<Integer, Queue<Integer>> map = new HashMap<>();
+        int[] inwardEdges = new int[numCourses];
 
-        for (int[] prerequisite : prerequisites) {
-            int start = prerequisite[1], end = prerequisite[0];
-            List<Integer> list = mapping.getOrDefault(start, new ArrayList<>());
-            list.add(end);
-            mapping.put(start, list);
-            inward[end]++;
+        for (int[] prereq : prerequisites) {
+            int start = prereq[1], end = prereq[0];
+            inwardEdges[end]++;
+            Queue<Integer> tempQueue = map.getOrDefault(start, new LinkedList<>());
+            tempQueue.add(end);
+            map.put(start, tempQueue);
         }
 
         Queue<Integer> queue = new LinkedList<>();
+        int[] solution = new int[numCourses];
+        int solutionIndex = 0;
 
         for (int index = 0; index < numCourses; index++) {
-            if (inward[index] == 0) {
+            if (inwardEdges[index] == 0) {
                 queue.add(index);
             }
         }
 
-        int[] answer = new int[numCourses];
-        int index = 0;
-
         while (!queue.isEmpty()) {
-            int next = queue.poll();
-            answer[index++] = next;
-            List<Integer> list = mapping.getOrDefault(next, new ArrayList<>());
-            for (int val : list) {
-                inward[val]--;
-                if (inward[val] == 0) {
-                    queue.add(val);
+            int course = queue.poll();
+            solution[solutionIndex++] = course;
+            Queue<Integer> tempQueue = map.getOrDefault(course, new LinkedList<>());
+            while (!tempQueue.isEmpty()) {
+                int next = tempQueue.poll();
+                inwardEdges[next]--;
+                if (inwardEdges[next] == 0) {
+                    queue.add(next);
                 }
             }
         }
 
-        if (index == numCourses) {
-            return answer;
+        if (solutionIndex == numCourses) {
+            return solution;
         }
 
         return new int[0];
