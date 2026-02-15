a6490105915ff8d1574d53b17b126fbc2044cbfb
Esteban Kim <47050615+Esteban-Kim@users.noreply.github.com>
2026-02-14 21:15:22 -0700

    Time: 29 ms (78.80%) | Memory: 56.8 MB (71.43%) - LeetSync

--- 1165-meeting-scheduler/meeting-scheduler.java
+++ 1165-meeting-scheduler/meeting-scheduler.java
@@ -1,22 +1,24 @@
 class Solution {
     public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
-        Arrays.sort(slots1, (a,b) -> a[0]-b[0]);
-        Arrays.sort(slots2, (a,b) -> a[0]-b[0]);
+        Arrays.sort(slots1, (a, b) -> a[0]-b[0]);
+        Arrays.sort(slots2, (a, b) -> a[0]-b[0]);
         
-        int slots1Index = 0, slots2Index = 0;
+        int s1Index = 0, s1Length = slots1.length, s2Index = 0, s2Length = slots2.length;
 
-        while (slots1Index < slots1.length && slots2Index < slots2.length) {
-            int startIntersection = Math.max(slots1[slots1Index][0], slots2[slots2Index][0]);
-            int endIntersection = Math.min(slots1[slots1Index][1], slots2[slots2Index][1]);
+        while (s1Index < s1Length && s2Index < s2Length) {
+            int s1Start = slots1[s1Index][0], s1End = slots1[s1Index][1];
+            int s2Start = slots2[s2Index][0], s2End = slots2[s2Index][1];
+
+            int startIntersection = Math.max(s1Start, s2Start), endIntersection = Math.min(s1End, s2End);
 
             if (endIntersection-startIntersection >= duration) {
                 return Arrays.asList(startIntersection, startIntersection+duration);
             }
 
-            if (slots2[slots2Index][1] < slots1[slots1Index][1]) {
-                slots2Index++;
+            if (s1End < s2End) {
+                s1Index++;
             } else {
-                slots1Index++;
+                s2Index++;
             }
         }
 
