13eaa9f6630f6e9df4ad545c7904a9bf40f91683
Esteban Kim <47050615+Esteban-Kim@users.noreply.github.com>
2026-02-15 07:57:02 -0700

    Time: 364 ms (5.02%) | Memory: 47.7 MB (33.67%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,39 +1,54 @@
 class Solution {
     public String minWindow(String s, String t) {
-        Map<Character, Integer> tFrequency = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            tFrequency.put(c, tFrequency.getOrDefault(c, 0)+1);
-        }
+        int begIndex = 0, endIndex = 0, sLength = s.length();
 
-        String minString = "";
-        int minWindow = Integer.MAX_VALUE, left = 0, right = 0, length = s.length();
+        Map<Character, Integer> targetFrequency = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            targetFrequency.put(c, targetFrequency.getOrDefault(c, 0)+1);
+        }
 
         Map<Character, Integer> sFrequency = new HashMap<>();
 
-        while (right < length) {
-            char c = s.charAt(right);
-            sFrequency.put(c, sFrequency.getOrDefault(c, 0)+1);
-            while (valid(tFrequency, sFrequency)) {
-                int totalLength = right-left+1;
-                if (totalLength < minWindow) {
-                    minWindow = totalLength;
-                    minString = s.substring(left, right+1);
+        String solution = "";
+        int solutionLength = Integer.MAX_VALUE;
+        
+        /**
+        step 1: increment endIndex until we find a solution
+        Then we want to decrement begIndex as long as there is a solution
+        If no solution exists then we continue back with step 1
+         */
+        while (endIndex < sLength) {
+            char current = s.charAt(endIndex++);
+            sFrequency.put(current, sFrequency.getOrDefault(current, 0)+1);
+            if (containsT(sFrequency, targetFrequency)) {
+                int currentLength = endIndex-begIndex;
+                if (solutionLength > currentLength) {
+                    solutionLength = Math.min(solutionLength, endIndex-begIndex);
+                    solution = s.substring(begIndex, endIndex);
                 }
-                char remove = s.charAt(left++);
-                sFrequency.put(remove, sFrequency.get(remove)-1);
+                
+                while (containsT(sFrequency, targetFrequency)) {
+                    currentLength = endIndex-begIndex;
+                    if (solutionLength > currentLength) {
+                        solutionLength = Math.min(solutionLength, endIndex-begIndex);
+                        solution = s.substring(begIndex, endIndex);
+                    }
+                    char begCharacter = s.charAt(begIndex++);
+                    sFrequency.put(begCharacter, sFrequency.getOrDefault(begCharacter, 0)-1);
+                }
             }
-            right++;
         }
 
-        return minString;
+        return solution;
     }
 
-    public boolean valid(Map<Character, Integer> tFrequency, Map<Character, Integer> sFrequency) {
-        for (Map.Entry<Character, Integer> entry : tFrequency.entrySet()) {
-            if (entry.getValue() > sFrequency.getOrDefault(entry.getKey(), 0)) {
+    public boolean containsT(Map<Character, Integer> sFrequency, Map<Character, Integer> targetFrequency) {
+        for (Map.Entry<Character, Integer> entry : targetFrequency.entrySet()) {
+            if (sFrequency.getOrDefault(entry.getKey(), 0)-entry.getValue() < 0) {
                 return false;
             }
         }
+
         return true;
     }
 }
