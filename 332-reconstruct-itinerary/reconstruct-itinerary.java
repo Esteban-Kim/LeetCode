03c148cad32b4384b647d287722a7b4958216d28
Esteban Kim <47050615+Esteban-Kim@users.noreply.github.com>
2026-02-15 08:12:01 -0700

    Time: 5 ms (99.79%) | Memory: 47 MB (85.17%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -1,28 +1,29 @@
 class Solution {
     public List<String> findItinerary(List<List<String>> tickets) {
-        List<String> path = new ArrayList<>();
-        Map<String, PriorityQueue<String>> map = new HashMap<>();
+        Map<String, PriorityQueue<String>> paths = new HashMap<>();
 
         for (List<String> ticket : tickets) {
-            String start = ticket.get(0), end = ticket.get(1);
-            PriorityQueue<String> temp = map.getOrDefault(start, new PriorityQueue<>());
-            temp.add(end);
-            map.put(start, temp);
+            String origin = ticket.get(0), destination = ticket.get(1);
+            PriorityQueue<String> pQueue = paths.getOrDefault(origin, new PriorityQueue<>());
+            pQueue.add(destination);
+            paths.put(origin, pQueue);
         }
 
-        dfs("JFK", map, path);
+        List<String> itinerary = new ArrayList<>();
 
-        return path;
+        dfs("JFK", paths, itinerary);
+
+        return itinerary;
     }
 
-    public void dfs(String airport, Map<String, PriorityQueue<String>> map, List<String> path) {
-        PriorityQueue<String> pQueue = map.getOrDefault(airport, new PriorityQueue<>());
+    public void dfs(String airport, Map<String, PriorityQueue<String>> paths, List<String> itinerary) {
+        PriorityQueue<String> pQueue = paths.getOrDefault(airport, new PriorityQueue<>());
 
         while (!pQueue.isEmpty()) {
-            String next = pQueue.poll();
-            dfs(next, map, path);
+            String nextAirport = pQueue.poll();
+            dfs(nextAirport, paths, itinerary);
         }
 
-        path.addFirst(airport);
+        itinerary.addFirst(airport);
     }
 }
