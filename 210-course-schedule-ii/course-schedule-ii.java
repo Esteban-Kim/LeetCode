573f11af2f65f137ab2ffda241de65f16ab281e4
Esteban Kim <47050615+Esteban-Kim@users.noreply.github.com>
2026-02-16 09:21:19 -0700

    Time: 7 ms (35.79%) | Memory: 48.1 MB (20.30%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,41 +1,42 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        Map<Integer, Queue<Integer>> map = new HashMap<>();
-        int[] inwardEdges = new int[numCourses];
+        Map<Integer, Queue<Integer>> prerequisiteMap = new HashMap<>();
+        int[] totalPrerequisites = new int[numCourses];
 
-        for (int[] prereq : prerequisites) {
-            int start = prereq[1], end = prereq[0];
-            inwardEdges[end]++;
-            Queue<Integer> tempQueue = map.getOrDefault(start, new LinkedList<>());
+        for (int[] prerequisite : prerequisites) {
+            int start = prerequisite[1], end = prerequisite[0];
+            totalPrerequisites[end]++;
+            Queue<Integer> tempQueue = prerequisiteMap.getOrDefault(start, new LinkedList<>());
             tempQueue.add(end);
-            map.put(start, tempQueue);
+            prerequisiteMap.put(start, tempQueue);
         }
 
-        Queue<Integer> queue = new LinkedList<>();
-        int[] solution = new int[numCourses];
-        int solutionIndex = 0;
+        Queue<Integer> nonPrerequisiteCourses = new LinkedList<>();
 
         for (int index = 0; index < numCourses; index++) {
-            if (inwardEdges[index] == 0) {
-                queue.add(index);
+            if (totalPrerequisites[index] == 0) {
+                nonPrerequisiteCourses.add(index);
             }
         }
 
-        while (!queue.isEmpty()) {
-            int course = queue.poll();
-            solution[solutionIndex++] = course;
-            Queue<Integer> tempQueue = map.getOrDefault(course, new LinkedList<>());
-            while (!tempQueue.isEmpty()) {
-                int next = tempQueue.poll();
-                inwardEdges[next]--;
-                if (inwardEdges[next] == 0) {
-                    queue.add(next);
+        int[] courseOrder = new int[numCourses];
+        int courseOrderIndex = 0;
+
+        while (!nonPrerequisiteCourses.isEmpty()) {
+            int addCourse = nonPrerequisiteCourses.poll();
+            courseOrder[courseOrderIndex++] = addCourse;
+            Queue<Integer> removeList = prerequisiteMap.getOrDefault(addCourse, new LinkedList<>());
+            while (!removeList.isEmpty()) {
+                int removeCourse = removeList.poll();
+                totalPrerequisites[removeCourse]--;
+                if (totalPrerequisites[removeCourse] == 0) {
+                    nonPrerequisiteCourses.add(removeCourse);
                 }
             }
         }
 
-        if (solutionIndex == numCourses) {
-            return solution;
+        if (courseOrderIndex == numCourses) {
+            return courseOrder;
         }
 
         return new int[0];
