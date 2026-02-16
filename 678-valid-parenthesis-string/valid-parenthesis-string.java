6937c50ac74699e48080ce70b8596c07a2a1a234
Esteban Kim <47050615+Esteban-Kim@users.noreply.github.com>
2026-02-16 09:46:32 -0700

    Time: 1 ms (31.29%) | Memory: 42.7 MB (68.69%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,34 +1,33 @@
 class Solution {
     public boolean checkValidString(String s) {
-        Stack<Integer> leftBracket = new Stack<>();
-        Stack<Integer> asterikBracket = new Stack<>();
+        Stack<Integer> lefts = new Stack<>();
+        Stack<Integer> asteriks = new Stack<>();
 
         for (int index = 0; index < s.length(); index++) {
             char c = s.charAt(index);
             if (c == '(') {
-                leftBracket.push(index);
+                lefts.push(index);
             } else if (c == '*') {
-                asterikBracket.push(index);
+                asteriks.push(index);
             } else {
-                if (!leftBracket.isEmpty()) {
-                    leftBracket.pop();
-                } else if (!asterikBracket.isEmpty()) {
-                    asterikBracket.pop();
+                if (!lefts.isEmpty()) {
+                    lefts.pop();
+                } else if (!asteriks.isEmpty()) {
+                    asteriks.pop();
                 } else {
                     return false;
                 }
             }
         }
 
-        while (!leftBracket.isEmpty() && !asterikBracket.isEmpty()) {
-            if (leftBracket.peek() < asterikBracket.peek()) {
-                leftBracket.pop();
-                asterikBracket.pop();
-            } else {
+        while (!lefts.isEmpty() && !asteriks.isEmpty()) {
+            if (lefts.peek() > asteriks.peek()) {
                 return false;
             }
+            lefts.pop();
+            asteriks.pop();
         }
 
-        return leftBracket.isEmpty();
+        return lefts.isEmpty();
     }
 }
