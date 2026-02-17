15f916da28154745f20435e77bbe96924ffa65f5
Esteban Kim <47050615+Esteban-Kim@users.noreply.github.com>
2026-02-17 07:08:56 -0700

    Time: 23 ms (87.42%) | Memory: 180.9 MB (91.39%) - LeetSync

--- 1952-minimum-sideway-jumps/minimum-sideway-jumps.java
+++ 1952-minimum-sideway-jumps/minimum-sideway-jumps.java
@@ -1,18 +1,16 @@
 class Solution {
     public int minSideJumps(int[] obstacles) {
-        int[] jumps = {1,0,1};
+        int[] jumps = {1, 0, 1};
+
         for (int obstacle : obstacles) {
             if (obstacle > 0) {
                 jumps[obstacle-1] = 1000000;
             }
-            for (int i = 0; i < 3; i++) {
-                if (obstacle-1 != i) {
-                    int firstJump = (i+1) % 3;
-                    int secondJump = (i+2) % 3;
-                    int firstValue = jumps[firstJump];
-                    int secondValue = jumps[secondJump];
-
-                    jumps[i] = Math.min(jumps[i], Math.min(firstValue, secondValue)+1);
+            for (int index = 0; index < 3; index++) {
+                if (obstacle-1 != index) {
+                    int firstJumpIndex = (index+1)%3, secondJumpIndex = (index+2)%3;
+                    int firstJumpValue = jumps[firstJumpIndex], secondJumpValue = jumps[secondJumpIndex];
+                    jumps[index] = Math.min(jumps[index], Math.min(firstJumpValue, secondJumpValue)+1);
                 }
             }
         }
