55fc2ad0be7f78d7cee606a824a1adbcc0ba1fab
Esteban Kim <47050615+Esteban-Kim@users.noreply.github.com>
2026-02-17 07:10:02 -0700

    Time: 0 ms (100.00%) | Memory: 42.3 MB (66.36%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,11 +1,12 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        return recursive(n, k)+1;
+        return findWinner(n, k)+1;
     }
-    public int recursive(int n, int k) {
+
+    public int findWinner(int n, int k) {
         if (n == 1) {
             return 0;
         }
-        return (recursive(n-1, k)+k)%n;
+        return (findWinner(n-1, k)+k) % n;
     }
 }
