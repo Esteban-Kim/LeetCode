46b3ee555f9980e809245c4b0720112863b7df32
Esteban Kim <47050615+Esteban-Kim@users.noreply.github.com>
2026-02-18 15:02:02 -0700

    Time: 20 ms (78.38%) | Memory: 51 MB (35.63%) - LeetSync

--- 1967-longest-substring-of-all-vowels-in-order/longest-substring-of-all-vowels-in-order.java
+++ 1967-longest-substring-of-all-vowels-in-order/longest-substring-of-all-vowels-in-order.java
@@ -1,20 +1,18 @@
 class Solution {
     public int longestBeautifulSubstring(String word) {
-        int count = 1;
-        int length = 1;
-        int maxLength = 0;
+        int count = 1, length = 1, maxLength = 0;
 
-        for (int i = 1; i < word.length(); i++) {
-            if (word.charAt(i-1) == word.charAt(i)) {
+        for (int index = 1; index < word.length(); index++) {
+            char first = word.charAt(index-1), second = word.charAt(index);
+            if (first == second) {
                 length++;
-            } else if (word.charAt(i-1) < word.charAt(i)) {
+            } else if (first < second) {
+                length++;
                 count++;
-                length++;
             } else {
                 count = 1;
                 length = 1;
             }
-
             if (count == 5) {
                 maxLength = Math.max(maxLength, length);
             }
