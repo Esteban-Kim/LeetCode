95fd09bb4642dec88465099cf058dc7c6b387ac0
Esteban Kim <47050615+Esteban-Kim@users.noreply.github.com>
2026-02-18 15:18:31 -0700

    Time: 0 ms (100.00%) | Memory: 42.2 MB (84.20%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,12 +1,12 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        return findWinner(n, k)+1;
+        return helper(n, k)+1;
     }
 
-    public int findWinner(int n, int k) {
+    public int helper(int n, int k) {
         if (n == 1) {
             return 0;
         }
-        return (findWinner(n-1, k)+k) % n;
+        return (helper(n-1, k) + k) % n;
     }
 }
