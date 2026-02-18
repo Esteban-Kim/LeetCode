400567390a3f5fe72e93f96c5b035760c00138fa
Esteban Kim <47050615+Esteban-Kim@users.noreply.github.com>
2026-02-18 15:10:04 -0700

    Time: 29 ms (79.62%) | Memory: 57.3 MB (30.33%) - LeetSync

--- 1165-meeting-scheduler/meeting-scheduler.java
+++ 1165-meeting-scheduler/meeting-scheduler.java
@@ -2,27 +2,25 @@ class Solution {
     public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
         Arrays.sort(slots1, (a, b) -> a[0]-b[0]);
         Arrays.sort(slots2, (a, b) -> a[0]-b[0]);
+        int s1Index = 0, s2Index = 0, s1Length = slots1.length, s2Length = slots2.length;
 
-        int firstIndex = 0, firstLength = slots1.length, secondIndex = 0, secondLength = slots2.length;
+        while (s1Index < s1Length && s2Index < s2Length) {
+            int s1Start = slots1[s1Index][0], s1End = slots1[s1Index][1];
+            int s2Start = slots2[s2Index][0], s2End = slots2[s2Index][1];
 
-        while (firstIndex < firstLength && secondIndex < secondLength) {
-            int firstStart = slots1[firstIndex][0], firstEnd = slots1[firstIndex][1];
-            int secondStart = slots2[secondIndex][0], secondEnd = slots2[secondIndex][1];
+            int start = Math.max(s1Start, s2Start);
+            int end = Math.min(s1End, s2End);
 
-            int firstMax = Math.max(firstStart, secondStart);
-            int firstMin = Math.min(firstEnd, secondEnd);
-
-            if (firstMin-firstMax >= duration) {
-                return Arrays.asList(firstMax, firstMax+duration);
+            if (end-start >= duration) {
+                return Arrays.asList(start, start+duration);
             }
 
-            if (firstEnd < secondEnd) {
-                firstIndex++;
+            if (s1End < s2End) {
+                s1Index++;
             } else {
-                secondIndex++;
+                s2Index++;
             }
         }
-        
 
         return new ArrayList<>();
     }
