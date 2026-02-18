35d8e5d47435318708609e8803ab04beb85b9186
Esteban Kim <47050615+Esteban-Kim@users.noreply.github.com>
2026-02-18 15:36:26 -0700

    Time: 189 ms (6.82%) | Memory: 74.2 MB (5.39%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,29 +1,23 @@
 class Solution {
     public int shortestPath(int[][] grid, int k) {
-        // row = 0, column = 1, steps = 2, k = 3
-
-        Queue<State> queue = new LinkedList<>();
-        Set<State> visited = new HashSet<>();
-        queue.add(new State(0, 0, 0, k));
-        visited.add(new State(0, 0, 0, k));
-
-        int[][] directions = {{-1,0},{0,-1},{1,0},{0,1}};
+        Set<Position> visited = new HashSet<>();
+        Queue<Position> queue = new LinkedList<>();
+        queue.add(new Position(0, 0, 0, k));
+        visited.add(new Position(0, 0, 0, k));
+        int[][] directions = {{-1,0}, {0,-1}, {0,1}, {1,0}};
         int rowLength = grid.length, columnLength = grid[0].length;
 
         while (!queue.isEmpty()) {
-            State next = queue.poll();
-            if (rowLength-1 == next.row && columnLength-1 == next.column) {
-                return next.steps;
+            Position temp = queue.poll();
+            if (temp.row == rowLength-1 && temp.column == columnLength-1) {
+                return temp.steps;
             }
             for (int[] direction : directions) {
-                int newRow = direction[0]+next.row, newColumn = direction[1]+next.column;
-                if (newRow >= 0 && newRow < rowLength && newColumn >= 0 && newColumn < columnLength) {
-                    State newState = new State(newRow, newColumn, next.steps+1, next.k-grid[newRow][newColumn]);
-                    if (next.k-grid[newRow][newColumn] >= 0 && !visited.contains(newState)) {
-                        visited.add(newState);
-                        queue.add(newState);
-                    }
-                } 
+                int newRow = direction[0]+temp.row, newColumn = direction[1]+temp.column, newSteps = temp.steps+1;
+                if (newRow >= 0 && newRow < rowLength && newColumn >= 0 && newColumn < columnLength && temp.k-grid[newRow][newColumn] >= 0 && !visited.contains(new Position(newRow, newColumn, newSteps, temp.k-grid[newRow][newColumn]))) {
+                    visited.add(new Position(newRow, newColumn, newSteps, temp.k-grid[newRow][newColumn]));
+                    queue.add(new Position(newRow, newColumn, newSteps, temp.k-grid[newRow][newColumn]));
+                }
             }
         }
 
@@ -31,12 +25,9 @@ class Solution {
     }
 }
 
-class State {
-    int row;
-    int column;
-    int steps;
-    int k;
-    public State(int row, int column, int steps, int k) {
+class Position {
+    int row, column, steps, k;
+    public Position(int row, int column, int steps, int k) {
         this.row = row;
         this.column = column;
         this.steps = steps;
@@ -44,16 +35,16 @@ class State {
     }
 
     @Override
-    public boolean equals(Object state) {
-        if (!(state instanceof State)) {
-            return false;
-        }
-        State temp = (State)state;
-        return (temp.row == this.row) && (temp.column == this.column) && (temp.k == this.k);
+    public int hashCode() {
+        return (this.row+1)*(this.column+1)*this.k;
     }
 
     @Override
-    public int hashCode() {
-        return (this.row+1)*(this.column+1)*this.k;
+    public boolean equals(Object object) {
+        if (!(object instanceof Position)) {
+            return false;
+        }
+        Position temp = (Position)object;
+        return temp.row == this.row && temp.column == this.column && temp.k == this.k;
     }
 }
